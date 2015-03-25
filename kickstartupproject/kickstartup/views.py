d3465deebb23cd9d4064ccc76ca9d1d3612b4bb1
John Collier <jmcollier139@gmail.com>
2015-03-24 20:35:38 -0400

    TWD Stuff

--- kickstartupproject/kickstartup/views.py
+++ kickstartupproject/kickstartup/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to KickStartUp!")
+    return HttpResponse("Welcome to KickStartUp! <br/> <a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("About. <a href="/rango/">Return to Index</a>")
 # Create your views here.
