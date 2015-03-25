501af2860b853f70ff4b474c552901be4acafff8
John Collier <jmcollier139@gmail.com>
2015-03-24 22:20:12 -0400

    Stupid typo

--- kickstartupproject/kickstartup/views.py
+++ kickstartupproject/kickstartup/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     context_dict = {'boldmessage': "I am bold font from the context"}
-    return render(request, 'rango/index.html', context_dict)
+    return render(request, 'kickstartup/index.html', context_dict)
 
 def about(request):
     return HttpResponse("About. <a href='/kickstartup/'>Return to Index</a>")
