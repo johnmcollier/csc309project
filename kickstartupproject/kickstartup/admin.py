912dfcc1222308ed9a7d4f5a7c0d0bb50b16ec84
John Collier <jmcollier139@gmail.com>
2015-03-29 19:26:31 -0400

    no message

--- kickstartupproject/kickstartup/admin.py
+++ kickstartupproject/kickstartup/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from kickstartup.models import UserProfile, Industry, StartUp, Keywords
 
+admin.site.register(UserProfile)
+admin.site.register(Industry)
+admin.site.register(StartUp)
+admin.site.register(Keywords)
 # Register your models here.
