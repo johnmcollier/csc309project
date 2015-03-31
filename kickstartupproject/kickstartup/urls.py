9ba81ff624c4605e247dd28ddc6863ce52059285
John Collier <jmcollier139@gmail.com>
2015-03-30 21:38:44 -0400

    Typo...

--- kickstartupproject/kickstartup/urls.py
+++ kickstartupproject/kickstartup/urls.py
@@ -5,4 +5,4 @@ urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^about/', views.about, name='about'),
         url(r'^register/', views.register, name='register'),
-        url(r'^login/', views.login, name='login'))
+        url(r'^login/', views.user_login, name='login'))
